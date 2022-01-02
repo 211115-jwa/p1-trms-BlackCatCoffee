be3138bb9dc70d7df029a0aab3c1741a1ed8193d
unknown <brett.dixon@revature.net>
2022-01-02 15:42:37 -0700

    service test

--- trms-back/src/test/java/com/revature/services/EmployeeServiceTest.java
+++ trms-back/src/test/java/com/revature/services/EmployeeServiceTest.java
@@ -15,13 +15,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.revature.beans.Comment;
 import com.revature.beans.Employee;
+import com.revature.beans.Reimbursement;
 import com.revature.data.CommentDAO;
 import com.revature.data.EmployeeDAO;
+import com.revature.data.ReimbursementDAO;
+import com.revature.utils.DAOFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
 	@Mock
-	private CommentDAO comDao;
+	private CommentDAO commentDao;
+	@Mock
+	private EmployeeDAO empDao;
+	@Mock
+	private ReimbursementDAO reqDao;
 	
 	@InjectMocks
 	private EmployeeService empServ = new EmployeeServiceImpl();
@@ -29,8 +36,29 @@ public class EmployeeServiceTest {
 	@Test
 	public void addNewCommentSuccessfully() {
 		Comment comment = new Comment();
-		when(comDao.create(comment)).thenReturn(10);
+		when(commentDao.create(comment)).thenReturn(10);
 		int newComment = empServ.addComment(comment);
 		assertNotEquals(0,newComment);
 	}
+	
+	@Test
+	public void getEmployeeByIdSuccessfully() {
+		Employee empl = new Employee();
+		empl.setEmpId(17);
+		
+		when(empDao.getById(17)).thenReturn(empl);
+		Employee actualEmp = empServ.getEmployeeById(17);
+		
+		assertEquals(empl,actualEmp);
+	}
+	
+	@Test
+	public void getsubmitReimbursementRequestsuccessfully() {
+		Reimbursement reim = new Reimbursement();
+		when(reqDao.create(reim)).thenReturn(0);
+		int newReim = empServ.submitReimbursementRequest(reim);
+		assertNotEquals(0,newReim);
+	}
+	
+	
 }
